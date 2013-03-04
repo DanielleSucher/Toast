d44fc5bc6a327a89a68d1d32a192bb59e03e08c0
Danielle Sucher <dsucher@gmail.com>
2013-03-04 02:42:07 -0500

    Add tab bar with history view and controller

---
+++ Toast/DESHistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  DESHistoryViewController.h
+//  Toast
+//
+//  Created by Danielle Sucher on 3/4/13.
+//  Copyright (c) 2013 Danielle Sucher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DESHistoryViewController : UIViewController
+
+@end
---
+++ Toast/DESHistoryViewController.m
@@ -0,0 +1,43 @@
+//
+//  DESHistoryViewController.m
+//  Toast
+//
+//  Created by Danielle Sucher on 3/4/13.
+//  Copyright (c) 2013 Danielle Sucher. All rights reserved.
+//
+
+#import "DESHistoryViewController.h"
+
+@interface DESHistoryViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *historyTextField;
+
+@end
+
+@implementation DESHistoryViewController
+
+
+
+#pragma mark
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
